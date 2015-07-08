720e7dae1ca99d6bf2b5f9a746185f84f4470575
Rusty Tanton <rusty@perfectsensedigital.com>
2015-07-07 21:33:55 -0400

    moved demo config to a separate file

---
+++ demo/config.js
@@ -0,0 +1,11 @@
+System.config({
+  defaultJSExtensions: true,
+  map: {
+  	'babel': '/assets/node/babel-core/browser.js',
+  	'bsp-form': '/assets/js/bsp-form.js',
+  	'bsp-form-message': '/assets/js/bsp-form-message.js',
+  	'bsp-utils': '/assets/bower/bsp-utils/bsp-utils.js',
+  	'jquery': '/assets/bower/jquery/dist/jquery.js'
+  },
+  transpiler: 'babel'
+});
