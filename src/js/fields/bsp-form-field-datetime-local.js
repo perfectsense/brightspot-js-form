233c370c64fe4a81c4aa02feb4697f60515f256c
Rusty Tanton <rusty@perfectsensedigital.com>
2015-08-12 13:48:25 -0400

    fixed datetime-local format

--- src/js/fields/bsp-form-field-datetime-local.js
+++ src/js/fields/bsp-form-field-datetime-local.js
@@ -4,11 +4,19 @@ import BspFormField from 'bsp-form-field';
 
 class BspFormFieldDatetimeLocal extends BspFormField {
 
+	addLeadingZero(num) {
+		if (num < 10) {
+			return "0" + num;
+		} else {
+			return num;
+		}
+	}
+
 	formattedInputTime(str) {
 		// input format is 1996-12-19T16:39:57
 		// @see http://www.w3.org/TR/html-markup/datatypes.html#form.data.datetime-local
 		var d = new Date(str);
-		return d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate() + 'T' + d.getHours() + ':' + d.getMinutes();
+		return d.getFullYear() + '-' + this.addLeadingZero( d.getMonth()+1 ) + '-' + this.addLeadingZero( d.getDate() ) + 'T' + this.addLeadingZero( d.getHours() ) + ':' + this.addLeadingZero( d.getMinutes() );
 	}
 
 	setDefaults() {
