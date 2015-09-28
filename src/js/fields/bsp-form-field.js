9d9ab04dd21a8882fce4eba2a0f69808c346ca96
Rusty Tanton <rusty@perfectsensedigital.com>
2015-09-28 12:29:37 -0400

    disabling usage of WeakMap because transpiler didn't seem to make it work in IE

--- src/js/fields/bsp-form-field.js
+++ src/js/fields/bsp-form-field.js
@@ -1,26 +1,34 @@
 import $ from 'jquery';
 
 /** @see http://ilikekillnerds.com/2015/02/what-are-weakmaps-in-es6/ */
-const privateData = new WeakMap();
+
+// disabling to see if it fixes IE bug
+// const privateData = new WeakMap();
+
+var data = {};
 
 export default class BspFormField {
 
 	get defaults() {
-		return privateData.get(this).defaults;
+		//return privateData.get(this).defaults;
+		return data.defaults;
 	}
 	set defaults(defaults = {}) {
-		privateData.set(this, { "defaults": defaults });
+		//privateData.set(this, { "defaults": defaults });
+		data.defaults = defaults;
 	}
 	setDefaults() {
 		return {};
 	}
 
 	get options() {
-		return privateData.get(this).options;
+		//return privateData.get(this).options;
+		return data.options;
 	}
 	set options(options) {
 		var merged = $.extend(true, {}, this.defaults, options);
-		privateData.set(this, { "options": merged });
+		//privateData.set(this, { "options": merged });
+		data.options = merged;
 	}
 
 	constructor($el, options = {}) {
